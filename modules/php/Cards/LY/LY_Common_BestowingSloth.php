866515396f4975bfebbed4c8cfd43e9c4827181a
Thomas Brian <taum.xd@gmail.com>
2026-04-30 00:50:23 -0400

    add bestowing sloth

--- modules/php/Cards/LY/LY_Common_BestowingSloth.php
+++ modules/php/Cards/LY/LY_Common_BestowingSloth.php
@@ -1,32 +1,36 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_BestowingSloth extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_EOLE_B_LY_113_C',
-            'asset'  => 'ALT_EOLE_B_LY_113_C',
-
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Bestowing Sloth"),
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_EOLE_B_LY_113_C',
+      'asset'  => 'ALT_EOLE_B_LY_113_C',
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Bestowing Sloth"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate(''),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate(''),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'ROC',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{H} I gain $<ASLEEP>.'),
- 				'supportDesc' => clienttranslate('{D} : <AFTER_YOU>. (End your turn as if you had played a card.)'),
- 			     'supportIcon' => 'discard',
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-];
+      'extension' => 'ROC',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{H} I gain $<ASLEEP>.'),
+      'supportDesc' => clienttranslate('{D} : <AFTER_YOU>. (End your turn as if you had played a card.)'),
+      'supportIcon' => 'discard',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'effectHand' => FT::GAIN(ME, ASLEEP),
+      'effectSupport' => FT::ACTION(AFTER_YOU, []),
+    ];
   }
 }
--- modules/php/Cards/MU/MU_Rare_BestowingSloth.php
+++ modules/php/Cards/MU/MU_Rare_BestowingSloth.php
@@ -1,33 +1,43 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_BestowingSloth extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_EOLE_B_LY_113_R2',
-            'asset'  => 'ALT_EOLE_B_LY_113_R',
-
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Bestowing Sloth"),
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_EOLE_B_LY_113_R2',
+      'asset'  => 'ALT_EOLE_B_LY_113_R',
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Bestowing Sloth"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate(''),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate(''),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'ROC',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{H} I gain <ASLEEP>.  #At Noon — <RESUPPLY>.#'),
- 				'supportDesc' => clienttranslate('{D} : <AFTER_YOU>.'),
- 			     'supportIcon' => 'discard',
-     'forest' => 3, 
-     'mountain' => 2, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'ROC',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{H} I gain <ASLEEP>.  #At Noon — <RESUPPLY>.#'),
+      'supportDesc' => clienttranslate('{D} : <AFTER_YOU>.'),
+      'supportIcon' => 'discard',
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'changedStats' => ['mountain'],
+      'effectHand' => FT::GAIN(ME, ASLEEP),
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'isMe',
+          'output' => FT::ACTION(RESUPPLY, [])
+        ],
+      ],
+      'effectSupport' => FT::ACTION(AFTER_YOU, []),
+    ];
   }
 }
