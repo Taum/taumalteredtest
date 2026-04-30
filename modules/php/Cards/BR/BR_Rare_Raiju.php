b16cb37fd0f9fc5f529046d5fe38c8fa7ae77cc0
Thomas Brian <taum.xd@gmail.com>
2026-04-30 00:34:19 -0400

    add Raiju

--- modules/php/Cards/BR/BR_Rare_Raiju.php
+++ modules/php/Cards/BR/BR_Rare_Raiju.php
@@ -2,29 +2,30 @@
 namespace ALT\Cards\BR;
 use ALT\Helpers\FT;
 
-class BR_Common_Raij extends \ALT\Models\Card
+class BR_Rare_Raiju extends \ALT\Models\Card
 {
   public function __construct($row){
 		parent::__construct($row);
         $this->properties = [
-            'uid' => 'ALT_EOLE_B_BR_106_C',
-            'asset'  => 'ALT_EOLE_B_BR_106_C',
+            'uid' => 'ALT_EOLE_B_BR_106_R1',
+            'asset'  => 'ALT_EOLE_B_BR_106_R',
 
     	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Raij?"),
+    	'rarity'  => RARITY_RARE,
+    	'name'  => clienttranslate("Raijū"),
       'typeline' => clienttranslate("Character - Animal"),
     	'type'  => CHARACTER,
     	'flavorText'  => clienttranslate(''),
       'artist' => "Jamin Amaral Fernandez",
 			'extension'=>'ROC',
    'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{H} I gain <FLEETING>. (If I would be sent to Reserve, discard me instead.)'),
+ 				'effectDesc' => clienttranslate('{H} I gain <FLEETING>.'),
      'forest' => 2, 
-     'mountain' => 1, 
+     'mountain' => 2, 
      'ocean' => 1, 
      'costHand' => 1, 
      'costReserve' => 1, 
+     'changedStats' => ['mountain'], 
      'effectHand' => FT::GAIN(ME, FLEETING),
 ];
   }
