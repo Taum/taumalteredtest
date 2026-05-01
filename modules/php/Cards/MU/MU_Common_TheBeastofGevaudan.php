058f8f49febfba07b3366564a04ea2af32f99e12
Thomas Brian <taum.xd@gmail.com>
2026-05-01 19:54:10 -0400

    Add The beast of Gevaudan (C/R/F)

--- modules/php/Cards/MU/MU_Common_TheBeastofGevaudan.php
+++ modules/php/Cards/MU/MU_Common_TheBeastofGevaudan.php
@@ -1,30 +1,38 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Common_TheBeastofGevaudan extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_EOLE_B_MU_114_C',
-            'asset'  => 'ALT_EOLE_B_MU_114_C',
-
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("The Beast of Gévaudan"),
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_EOLE_B_MU_114_C',
+      'asset'  => 'ALT_EOLE_B_MU_114_C',
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("The Beast of Gévaudan"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate(''),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate(''),
       'artist' => "Victor Canton",
-			'extension'=>'ROC',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{H} You may target another Character with {V} less than or equal to mine. It switches Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 3, 
-];
+      'extension' => 'ROC',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{H} You may target another Character with {V} less than or equal to mine. It switches Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER],
+        'excludeSelf' => true,
+        'compareTargetBiomeToSource' => ['biome' => FOREST, 'op' => 'lte'],
+        'effect' => FT::ACTION(MOVE_CARD, []),
+      ]),
+    ];
   }
 }
