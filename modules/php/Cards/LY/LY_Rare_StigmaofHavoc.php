5a59f821a8d15f905260ce347544a3fc0f79d4c1
Thomas Brian <taum.xd@gmail.com>
2026-05-01 18:35:01 -0400

    add LY Stigma of Havoc

--- modules/php/Cards/LY/LY_Rare_StigmaofHavoc.php
+++ modules/php/Cards/LY/LY_Rare_StigmaofHavoc.php
@@ -1,33 +1,44 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_StigmaofHavoc extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_EOLE_B_LY_115_R1',
-            'asset'  => 'ALT_EOLE_B_LY_115_R',
-
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Stigma of Havoc"),
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_EOLE_B_LY_115_R1',
+      'asset'  => 'ALT_EOLE_B_LY_115_R',
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Stigma of Havoc"),
       'typeline' => clienttranslate("Character - Corruption"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate(''),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate(''),
       'artist' => "Khoa Viet",
-			'extension'=>'ROC',
-   'subtypes'  => [CORRUPTION],
- 				'effectDesc' => clienttranslate('{H} Each player discards their Reserve, then #<RESUPPLIES>.#  #{J} <RESUPPLY>.#'),
- 				'supportDesc' => clienttranslate('{D} : Create an <AEROLITH> token in target player\'s Landmarks.'),
- 			     'supportIcon' => 'discard',
-     'forest' => 5, 
-     'mountain' => 5, 
-     'ocean' => 6, 
-     'costHand' => 5, 
-     'costReserve' => 5, 
-     'changedStats' => ['costHand'], 
-];
+      'extension' => 'ROC',
+      'subtypes'  => [CORRUPTION],
+      'effectDesc' => clienttranslate('{H} Each player discards their Reserve, then #<RESUPPLIES>.#  #{J} <RESUPPLY>.#'),
+      'supportDesc' => clienttranslate('{D} : Create an <AEROLITH> token in target player\'s Landmarks.'),
+      'supportIcon' => 'discard',
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 6,
+      'costHand' => 5,
+      'costReserve' => 5,
+      'changedStats' => ['costHand'],
+      'effectHand' => FT::SEQ(
+        FT::ACTION(SPECIAL_EFFECT, ['effect' => 'discardAllReserve']),
+        FT::ACTION(SPECIAL_EFFECT, ['effect' => 'eachPlayerResupply'])
+      ),
+      'effectPlayed' => FT::ACTION(RESUPPLY, []),
+      'effectSupport' =>  [
+        'action' => INVOKE_TOKEN,
+        'args' => ['tokenType' => 'NE_Common_Aerolith', 'targetLocation' => [LANDMARK]],
+      ],
+    ];
   }
 }
