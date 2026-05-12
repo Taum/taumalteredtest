51ed8a8712582619e6835a573415582b925edb67
Thomas Brian <taum.xd@gmail.com>
2026-05-12 02:52:43 -0400

    add option to skip images (default on)

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -12,7 +12,7 @@
 
 $gameinfos = [
   // Name of the game in English (will serve as the basis for translation)
-  'game_name' => 'Altered Preprod',
+  'game_name' => 'Altered',
 
   // Game designer (or game designers, separated by commas)
   'designer' => 'Equinox team',
